841d306ac82e595b8d08ea78f2fd1ac1073b09bb
Miras <miras.esenkeldy@gmail.com>
2024-06-28 18:40:07 +0200

    h1

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,7 @@ from .models import User
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(max_length=20, label = 'Name')
+    name = forms.CharField(max_length=200, label = 'Name')
     surname = forms.CharField(max_length=20, label = 'Surname')
     gender = forms.CharField( max_length=20)
     adult = forms.BooleanField()
