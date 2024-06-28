89f81c2818353c71bae7466d4fb9d0728deef485
Miras <miras.esenkeldy@gmail.com>
2024-06-28 18:38:41 +0200

    h1

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 app_name = 'users'
+#add some changes
 urlpatterns = [
     path("", views.index, name="index"),
     path("add_user/", views.add_user, name="add_user")
+
+    
 ]
